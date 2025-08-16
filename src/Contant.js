d4792b5a0bb42b310fe5a8d5ec979712dd422d6c
thangaraj54321 <tr5479510@gmail.com>
2025-08-16 20:28:22 +0530

    new commit

--- src/Contant.js
+++ src/Contant.js
@@ -1,50 +1,59 @@
-import { list } from "postcss";
+
 import React from "react";
 import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 
-
-
 const Contant = () => {
-  const [items,setItems] = useState(
-    [
-      {id:1,
-        checked:false,
-        item:"thanga"
-      },
-    
-    {  id:2,
-      checked:true,
-      item:"roja"
+  const [items, setItems] = useState([
+    { id: 1,
+       checked: true,
+       item: "thanga" },
+
+    { id: 2, 
+      checked: true,
+       item: "sumathi" },
+    {
+      id: 3,
+      checked: true,
+      item: "playing Vollyball",
     },
-   {
-    id:3,
-    checked:true,
-    item:"playing Vollyball"
-   }]);
+  ]);
+  const handleCheck = (id) => {
+    const listItem = items.map((item) => item.
+    id === id ? { checked:!item.checked} : item )
+    setItems(listItem)
+    localStorage.setItem("todo_list", JSON.stringify
+      (listItem)
+    )
+  };
+  const handleDelete = (id) =>{
+    const listItem = items.filter((item) =>
+    item.id!==id)
+    setItems(listItem)
+  }
   return (
     <main>
-         <ul>
-             {items.map((item) =>(
-            <li className="item" key={item.id}>
-              <input 
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
               type="checkbox"
-              checked = {item.checked}
-              />
-              <label>{item.item}</label>
-              <FaTrashAlt 
-                 role = "button"
-                 tabIndex="0"
-              />
-            </li>
-           ))}
-         
-
-         </ul>
-     
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label
+           
+             onDoubleClick={() => handleCheck(item.id)
+             }>{item.item}</label>
+            <FaTrashAlt 
+            role="button" 
+            onClick={() =>handleDelete(item.id)}
+            tabIndex="0" />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
-
 
 export default Contant;
